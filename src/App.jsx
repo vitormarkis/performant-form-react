5d90ce07e9c81fc2f6a27868249a797dbdb9faab
Vitor Markis <vitormarkis2369@gmail.com>
2024-01-11 21:36:51 -0300

    add validation list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ const initialInputRef = {
   element: null,
   value: "",
   listeners: [],
-  error: null,
+  error: {
+    message: null,
+    code: null,
+  },
 }
 
 function App() {
@@ -21,20 +24,15 @@ function App() {
     function listener(e) {
       const newValue = e.target.value
       inputRef.current.value = newValue
-      if (newValue.length < 3) {
-        if (ref.error) return
-        const errorMsg = "Muito curto."
-        setError(errorMsg)
-        inputRef.current.error = errorMsg
-        return
-      }
-      if (!ref.error) return
-      setError(null)
-      inputRef.current.error = null
+      validateFieldValue(newValue, ref.error, async error => {
+        setError(error.message)
+        inputRef.current.error.code = error.code
+        inputRef.current.error.message = error.message
+      })
     }
 
     if (ref.listeners.length === 0 || !ref.listeners.includes(listener)) {
-      ref.element.addEventListener("change", listener)
+      ref.element.addEventListener("input", listener)
       ref.listeners.push(listener)
     }
 
@@ -63,3 +61,46 @@ function App() {
 }
 
 export default App
+
+/**
+ * Validations
+ */
+const validations = [
+  value => ({
+    isValid: value.length < 12,
+    message: "Muito longo.",
+    code: "TOO_LONG",
+  }),
+  value => ({
+    isValid: value.length > 4,
+    message: "Muito curto.",
+    code: "TOO_SHORT",
+  }),
+  value => ({
+    isValid: value.includes("@"),
+    message: "Deve incluir @",
+    code: "SHOULD_INCLUDE_AT",
+  }),
+  value => ({
+    isValid: value.startsWith("mrks"),
+    message: "Deve começar com 'mrks'.",
+    code: "STARTS_WITH_A",
+  }),
+]
+
+function validateFieldValue(value, error, onError) {
+  for (const validate of validations) {
+    const { isValid, message, code } = validate(value)
+    if (isValid) continue
+    if (code === error.code) return
+    onError({ message, code })
+    return
+  }
+
+  const hasError = error.code != null
+  if (!hasError) return
+  onError({
+    message: null,
+    code: null,
+  })
+}
